59dff1d01bf6a55751c63139c0cd427bdc2c14c2
adeelshah110 <himatshah@quest.edu.pk>
2023-09-02 00:03:02 +0300

    change

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pandas as pd
+import matplotlib
+
 df = pd.read_csv("C:/mt/dataset/Avocado.csv")
 df = df.copy()[df["type"]=="organic"]
 df["Date"] = pd.to_datetime(df["Date"])
-
-
 df.sort_values(by= "Date", ascending=True, inplace=True)
-df["Date"] = pd.to_datetime(df["Date"])
-alb_df = df[ df["region"]== "Albany"]
-alb_df = alb_df.set_index("Date")
-alb_df.sort_index(inplace =True)
-print(df.head())
+graph_df = pd.DataFrame()
+for region in df["region"].unique():
+    #print (region)
+    region_df = df.copy()[df["region"]== region]
+    region_df.set_index("Date", inplace=True)
+    region_df.sort_index(inplace=True)
+    region_df[f'{region}_price25ma']= region_df["AveragePrice"].rolling(25).mean()
+    
+    if graph_df.empty:
+        graph_df= region_df[[f'{region}_price25ma']]
+    else:
+        graph_df = graph_df.join(region_df[f'{region}_price25ma'])
+graph_df.plot(figsize=(8,5), legend=False)
